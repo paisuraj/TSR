f3a289c51a8b6393c908d43b12fcfc6dd5fc77b9
Suraj Pai <pai.suraj2u@gmail.com>
2022-10-23 16:53:31 -0500

    commit -m "created GitClone script"

--- GitCloneScript.py
+++ GitCloneScript.py
@@ -1,24 +1,19 @@
-# file = open('ProjectsList.sv', 'r')
-# project = file.readline()
-# print(project)
-# # while file != '':
-# #     print(project)
-# #     project = file.readline()
-
 
 import csv
-#from git.repo.base import Repo
 import os
-from src import test
 
 
-with open('test.csv') as file_obj:
+with open('CSV/Projects.csv') as file_obj:
     reader_obj = csv.reader(file_obj)
-    fileCount = 0
     for row in reader_obj:
         cmd = "git clone {}".format(row[0])
         print("Starting to clone {}".format(row[0]))
+        proj = row[0].split('/')[-1]
+        copyOver = "cp -R " + "./" + proj + " ./Projects/"
         os.system(cmd)
+        os.system(copyOver)
+        os.system("rm -rf " + proj)
+        #os.system('mv' + cmd + 'Projects')
         print("Finished cloning {}".format(row[0]))
         print("#############################")
         print("")
