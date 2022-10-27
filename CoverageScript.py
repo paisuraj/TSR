31899ec6a35a09036075e091ac4d3ee6d351dc69
Suraj Pai <pai.suraj2u@gmail.com>
2022-10-27 15:20:50 -0500

    commit -m "adding reduce.py"

--- CoverageScript.py
+++ CoverageScript.py
@@ -1,5 +1,10 @@
 
-with open('Projects/') as file_obj:
-    
+#with open('Projects/') as file_obj:
 
-
+import os
+install_reqs = 'pipreqs'
+os.system(install_reqs)
+run_reqs = 'pip install -r requirements.txt'
+os.system(run_reqs)
+run_coverage = 'coverage run - m pytest'
+os.system(run_coverage)
