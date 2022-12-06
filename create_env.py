2ef9f5280072682d51150ef4eab503e3fcdc893c
Suraj Pai <pai.suraj2u@gmail.com>
2022-12-06 17:12:42 -0600

    script to generate virtual env

---
+++ create_env.py
@@ -0,0 +1,12 @@
+#this file is meant to create the virtual environment
+import os
+
+
+
+os.system('py -m venv env')
+os.system('.\env\Scripts\activate')
+
+
+os.system('pip install coverage')
+os.system('pip install pytest')
+os.system('pip install -r requirements.txt')
